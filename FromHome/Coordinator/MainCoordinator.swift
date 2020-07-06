5aded18d725542ff8d37c9675a4a10f241f258e3
thatdaniboy <thatdaniboy@icloud.com>
2020-07-06 17:39:37 -0300

    implement coordinator

---
+++ FromHome/Coordinator/MainCoordinator.swift
@@ -0,0 +1,30 @@
+//
+//  MainCoordinator.swift
+//  FromHome
+//
+//  Created by Daniboy on 7/6/20.
+//  Copyright © 2020 biaplutarco. All rights reserved.
+//
+
+import UIKit
+
+class MainCoordinator: Coordinator {
+
+    var childCoordinators = [Coordinator]()
+
+    var navigationController: UINavigationController!
+    var tabBarController: UITabBarController?
+
+    var window: UIWindow
+
+    init(withWindowScene windowScene: UIWindowScene) {
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        window.windowScene = windowScene
+
+        window.rootViewController = UITabBarController.init()
+        window.makeKeyAndVisible()
+    }
+
+    func start() {}
+}
