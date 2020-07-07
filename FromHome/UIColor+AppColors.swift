8aed1f2b54f71be1ee098350cacc4fafdbd0d19b
Bia <biaplutarco@gmail.com>
2020-07-06 22:33:41 -0300

    rename textfield to BackgroundTextfield

---
+++ FromHome/UIColor+AppColors.swift
@@ -0,0 +1,60 @@
+//
+//  UIColor+AppColors.swift
+//  FromHome
+//
+//  Created by Bia Plutarco on 06/07/20.
+//  Copyright © 2020 biaplutarco. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var backgroundAlert: UIColor {
+        return UIColor(named: "BackgroundAlert")!
+    }
+    
+    static var backgroundButton: UIColor {
+        return UIColor(named: "BackgroundButton")!
+    }
+    
+    static var gradientBottom: UIColor {
+        return UIColor(named: "GradientBottom")!
+    }
+    
+    static var gradientTop: UIColor {
+        return UIColor(named: "GradientTop")!
+    }
+    
+    static var lineAlert: UIColor {
+        return UIColor(named: "LineAlert")!
+    }
+    
+    static var lineBottom: UIColor {
+        return UIColor(named: "LineBottom")!
+    }
+    
+    static var lineTop: UIColor {
+        return UIColor(named: "LineTop")!
+    }
+    
+    static var primaryText: UIColor {
+        return UIColor(named: "PrimaryText")!
+    }
+    
+    static var secondaryText: UIColor {
+        return UIColor(named: "SecondaryText")!
+    }
+    
+    static var shadowBottom: UIColor {
+        return UIColor(named: "ShadowBottom")!
+    }
+    
+    static var shadowTop: UIColor {
+        return UIColor(named: "ShadowTop")!
+    }
+    
+    static var backgroundTextfield: UIColor {
+        return UIColor(named: "BackgroundTextfield")!
+    }
+}
