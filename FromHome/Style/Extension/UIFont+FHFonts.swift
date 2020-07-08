a3baba6cef65b6f8404816f044867df049ab364d
thatdaniboy <thatdaniboy@icloud.com>
2020-07-07 21:16:14 -0300

    remove some warnings from linter

--- FromHome/Style/Extension/UIFont+FHFonts.swift
+++ FromHome/Style/Extension/UIFont+FHFonts.swift
@@ -9,43 +9,43 @@
 import UIKit
 
 extension UIFont {
-    
+
     static var greeting: UIFont {
         return UIFont.systemFont(ofSize: 18)
     }
-    
+
     static var username: UIFont {
         return UIFont.boldSystemFont(ofSize: 36)
     }
-    
+
     static var titleCardHeader: UIFont {
         return UIFont.boldSystemFont(ofSize: 16)
     }
-    
+
     static var checklist: UIFont {
         return UIFont.systemFont(ofSize: 16)
     }
-    
+
     static var titleCardFooter: UIFont {
         return UIFont.boldSystemFont(ofSize: 21)
     }
-    
+
     static var subtitleCardFooter: UIFont {
         return UIFont.systemFont(ofSize: 16)
     }
-    
+
     static var bodyCardFooter: UIFont {
         return UIFont.systemFont(ofSize: 14)
     }
-    
+
     static var titleAlert: UIFont {
         return UIFont.boldSystemFont(ofSize: 16)
     }
-    
+
     static var titleButton: UIFont {
         return UIFont.boldSystemFont(ofSize: 16)
     }
-    
+
     static var destructiveTitleButton: UIFont {
         return UIFont.systemFont(ofSize: 16)
     }
--- FromHome/Style/Style.swift
+++ FromHome/Style/Style.swift
@@ -9,73 +9,73 @@
 import UIKit
 
 class Style {
-    
+
     enum TextStyle {
-        
+
         case greeting
         case username
-        
+
         case titleCardHeader
         case checklist
-        
+
         case titleCardFooter
         case subtitleCardFooter
         case bodyCardFooter
-        
+
         case titleAlert
-        
+
         case titleButton
         case destructiveTitleButton
     }
-    
+
     struct TextAttributes {
-        
+
         let font: UIFont
         let color: UIColor
         let backgroundColor: UIColor?
-        
+
         init(font: UIFont, color: UIColor, backgroundColor: UIColor? = nil) {
             self.font = font
             self.color = color
             self.backgroundColor = backgroundColor
         }
     }
-    
+
     let backgroundColor: UIColor
     let attributesForStyle: (_ style: TextStyle) -> TextAttributes
-    
+
     init(backgroundColor: UIColor, attributesForStyle: @escaping (_ style: TextStyle) -> TextAttributes) {
-        
+
         self.backgroundColor = backgroundColor
         self.attributesForStyle = attributesForStyle
     }
-    
+
     func font(for style: TextStyle) -> UIFont {
         return attributesForStyle(style).font
     }
-    
+
     func color(for style: TextStyle) -> UIColor {
         return attributesForStyle(style).color
     }
-    
+
     func backgroundColor(for style: TextStyle) -> UIColor? {
         return attributesForStyle(style).backgroundColor
     }
-    
+
     //  Applying Style to UI Elements
     func apply(textStyle: TextStyle, to label: UILabel) {
-        
+
         let attributes = attributesForStyle(textStyle)
-        
+
         label.font = attributes.font
         label.textColor = attributes.color
         label.backgroundColor = attributes.backgroundColor
     }
-    
+
     func apply(textStyle: TextStyle, to button: UIButton) {
-        
+
         let attributes = attributesForStyle(textStyle)
-       
+
         button.setTitleColor(attributes.color, for: .normal)
         button.titleLabel?.font = attributes.font
         button.backgroundColor = attributes.backgroundColor
