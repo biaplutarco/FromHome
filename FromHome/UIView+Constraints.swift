aad605c8f08c9fc6ffc0d17efd13a66cac432bc6
Bia <biaplutarco@gmail.com>
2020-07-07 21:02:52 -0300

    create fulfillsuperview

---
+++ FromHome/UIView+Constraints.swift
@@ -0,0 +1,27 @@
+//
+//  UIView+Constraints.swift
+//  FromHome
+//
+//  Created by Bia Plutarco on 07/07/20.
+//  Copyright © 2020 biaplutarco. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func fulfillSuperview() {
+        
+        guard let superview = self.superview else { return }
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            
+            topAnchor.constraint(equalTo: superview.topAnchor),
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor)
+        ])
+    }
+}
