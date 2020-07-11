f211a14233acf3a09a198653bd10c1fd74d17817
Bia <biaplutarco@gmail.com>
2020-07-11 20:37:29 -0300

    create greeting view model

---
+++ FromHome/View/Components/GreetingHeader/GreetingViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  GreetingViewModel.swift
+//  FromHome
+//
+//  Created by Bia Plutarco on 11/07/20.
+//  Copyright © 2020 biaplutarco. All rights reserved.
+//
+
+import Foundation
+
+struct GreetingViewModel {
+
+    var username: String
+}
