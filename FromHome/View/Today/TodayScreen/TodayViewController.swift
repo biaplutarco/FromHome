a322e0221810fff86323d5883cbe617c6966661f
thatdaniboy <thatdaniboy@icloud.com>
2020-07-22 18:11:09 -0300

    fix to help on merge

---
+++ FromHome/View/Today/TodayScreen/TodayViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TodayViewController.swift
+//  FromHome
+//
+//  Created by Daniboy on 7/22/20.
+//  Copyright © 2020 biaplutarco. All rights reserved.
+//
+
+import UIKit
+
+class TodayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
