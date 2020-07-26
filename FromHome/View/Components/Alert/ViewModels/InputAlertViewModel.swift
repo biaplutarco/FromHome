abb2c70cc47356797c6a7317f5f433a6a88ace7d
Bia <biaplutarco@gmail.com>
2020-07-25 22:40:40 -0300

    add new alert

--- FromHome/View/Components/Alert/ViewModels/InputAlertViewModel.swift
+++ FromHome/View/Components/Alert/ViewModels/InputAlertViewModel.swift
@@ -8,15 +8,25 @@
 
 import Foundation
 
-class ChangeNameAlertViewModel: AlertViewModel {
+enum InputAlertTitle: String {
+
+    case changeName = "How would you like to be call?"
+    case changeTask = "Write your new task"
+}
+
+class InputAlertViewModel: AlertViewModel {
 
     var type: AlertViewModelType { .input }
 
-    var title: String = "How would you like to be call?"
+    var title: String
 
     var leftButtonTitle: String = "Save"
 
     var bodyMessage: String?
 
     var rightButtonTitle: String? { "Cancel" }
+
+    init(_ title: InputAlertTitle) {
+        self.title = title.rawValue
+    }
 }
